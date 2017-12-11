64b363d88c725c92c91383ef95064dfcbc41960a
Sean Kennedy <totallynotsean@gmail.com>
2017-12-11 11:20:00 -0500

    Adjusting 404 page

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const NotFoundPage = () => (
   <div className="page-not-found">
-    <div className="content-container">
-      <h1>404</h1>
-    </div>
+    <section className="section section--spacing section--bg-sky-light">
+      <div className="content-container">
+        <h1 className="section__title">Whoops!</h1>
+        <div className="content">
+          <p>Something isn't quite right here, we weren't able to find what you were looking for.</p>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
