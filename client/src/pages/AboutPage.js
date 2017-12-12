8e8f8d217446f4122896f08a79cae1f69eec7f4b
Sean Kennedy <totallynotsean@gmail.com>
2017-12-12 13:22:59 -0500

    Updating about page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -4,8 +4,8 @@ export const AboutPage = () => (
   <div className="page-about">
     <div className="page-content">
       <section className="section section--spacing section--bg-sky-light">
+        <h1 className="section__title">About</h1>
         <div className="content-container">
-          <h1 className="section__title">About</h1>
           <div className="content">
             <p>
               Geopulse is a tool that allows you to see content based on your location including a Google Map of the
@@ -16,34 +16,10 @@ export const AboutPage = () => (
         </div>
       </section>
       <section className="section section--spacing section--bg-earth-light">
+        <h1 className="section__title">Privacy</h1>
         <div className="content-container">
-          <h1 className="section__title">Roadmap</h1>
-          <div className="content">
-            <p>(Roadmap)</p>
-          </div>
-        </div>
-      </section>
-      <section className="section section--spacing section--bg-earth-medium">
-        <div className="content-container">
-          <h1 className="section__title">Privacy</h1>
           <div className="content">
             <p>No data is saved on our servers and is only requested on an as-needed basis.</p>
-          </div>
-        </div>
-      </section>
-      <section className="section section--spacing section--bg-earth-dark">
-        <div className="content-container">
-          <h1 className="section__title">Thanks</h1>
-          <div className="content">
-            <p>(People)</p>
-          </div>
-        </div>
-      </section>
-      <section className="section section--spacing section--bg-earth-darker">
-        <div className="content-container">
-          <h1 className="section__title">Contact</h1>
-          <div className="content">
-            <p>(Info)</p>
           </div>
         </div>
       </section>
