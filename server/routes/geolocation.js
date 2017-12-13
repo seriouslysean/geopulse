3485f213e4d3560fd695e5fc7735267906554347
Sean Kennedy <totallynotsean@gmail.com>
2017-12-13 08:17:04 -0500

    Console log res

--- server/routes/geolocation.js
+++ server/routes/geolocation.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 module.exports = app => {
   app.get("/api/geolocation", async (req, res) => {
+    const defaults = {
+      latitude: 0,
+      longitude: 0
+    };
     let clientIp = res.ip;
+    console.log(res);
 
     if (process.env.NODE_ENV === "development") {
       try {
@@ -13,19 +18,19 @@ module.exports = app => {
       }
     }
 
-    try {
-      const data = await axios(`https://freegeoip.net/json/${clientIp}`);
-      const { latitude, longitude } = data.data;
-      res.send({
-        latitude,
-        longitude
-      });
-    } catch (error) {
-      console.error(error);
-      res.send({
-        latitude: 0,
-        longitude: 0
-      });
+    if (clientIp) {
+      try {
+        const data = await axios(`https://freegeoip.net/json/${clientIp}`);
+        const { latitude, longitude } = data.data;
+        res.send({
+          latitude,
+          longitude
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    res.send(defaults);
   });
 };
